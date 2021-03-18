1bd858191d29ca74332c6c435016baf91ebb97c2
caesargutierrez <karim-100gtz@hotmail.com>
2021-03-18 17:15:11 -0600

    First raw look of flow control

---
+++ GymTrack/GymTrack/Day.swift
@@ -0,0 +1,20 @@
+//
+//  Day.swift
+//  GymTrack
+//
+//  Created by Caesar Gutierrez on 3/18/21.
+//
+
+import Foundation
+
+class WorkoutDay {
+    var name = ""
+    var dayofweek: Int = 0
+    
+    init (day:String, dow: Int){
+        self.name = day
+        self.dayofweek = dow
+    }
+    
+    
+}
--- GymTrack/GymTrack/ViewController.swift
+++ GymTrack/GymTrack/ViewController.swift
@@ -7,11 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public let days = ["Monday","Tuesday"]
+
+class ViewController: UIViewController, UITableViewDataSource,
+                      UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let dayViewIdentifier = "dayView"
+    
+    /* Number of rows. */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return days.count
+    }
+    
+    /* Return the formatted cells */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: dayViewIdentifier, for: indexPath as IndexPath)
+        cell.textLabel?.text = days[indexPath.row]
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
